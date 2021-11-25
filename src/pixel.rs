d231b8b671ad1f9eefdf16295b25649ff4fa20b3
Chevy Ray Johnston <happytrash@gmail.com>
2021-11-25 13:42:29 -0800

    helper conversions for pixel

--- src/pixel.rs
+++ src/pixel.rs
@@ -34,4 +34,63 @@ impl Pixel {
     pub(crate) const fn hash(self) -> u8 {
         self.r ^ self.g ^ self.b ^ self.a
     }
+
+    /// Pack the pixel into a 32-bit RGBA integer.
+    #[inline]
+    pub fn pack(self) -> u32 {
+        (self.r as u32) << 24 | (self.g as u32) << 16 | (self.b as u32) << 8 | (self.a as u32)
+    }
+
+    /// Unpack the pixel from a 32-bit RGBA integer.
+    #[inline]
+    pub fn unpack(packed: u32) -> Self {
+        Self {
+            r: (packed >> 24) as u8,
+            g: (packed >> 16) as u8,
+            b: (packed >> 8) as u8,
+            a: packed as u8,
+        }
+    }
+}
+
+impl From<u32> for Pixel {
+    #[inline]
+    fn from(val: u32) -> Self {
+        Self::unpack(val)
+    }
+}
+
+impl Into<u32> for Pixel {
+    #[inline]
+    fn into(self) -> u32 {
+        self.pack()
+    }
+}
+
+impl From<(u8, u8, u8, u8)> for Pixel {
+    #[inline]
+    fn from((r, g, b, a): (u8, u8, u8, u8)) -> Self {
+        Self { r, g, b, a }
+    }
+}
+
+impl Into<(u8, u8, u8, u8)> for Pixel {
+    #[inline]
+    fn into(self) -> (u8, u8, u8, u8) {
+        (self.r, self.g, self.b, self.a)
+    }
+}
+
+impl From<[u8; 4]> for Pixel {
+    #[inline]
+    fn from([r, g, b, a]: [u8; 4]) -> Self {
+        Self { r, g, b, a }
+    }
+}
+
+impl Into<[u8; 4]> for Pixel {
+    #[inline]
+    fn into(self) -> [u8; 4] {
+        [self.r, self.g, self.b, self.a]
+    }
 }
